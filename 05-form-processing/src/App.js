a7d8f809586b2fae62e8f2137722e8a04babee2d
kunxin-chor <chorkunxin@yahoo.com>
2021-06-02 09:26:58 +0000

    Addd form processing example

---
+++ 05-form-processing/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// import in our form
+import Form from "./Form"
+
+function App() {
+  return (
+    <div className="container">
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 05-form-processing/src/Form.js
@@ -0,0 +1,38 @@
+import React from "react"
+
+export default class Form extends React.Component {
+
+    state = {
+        firstName: "",
+        lastName: ""
+    }
+
+    updateFirstName = (event) => {
+        this.setState({
+            firstName: event.target.value
+        })
+    }
+
+    updateLastName = (event) => {
+        this.setState({
+            lastName : event.target.value
+        })
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <div>
+                    <label className="form-label">First Name:</label>
+                    <input onChange={this.updateFirstName} type="text" value={this.state.firstName} name="firstName" className="form-control" />
+                </div>
+                <div>
+                    <label className="form-label">Last Name</label>
+                    <input onChange={this.updateLastName} className="form-control" type="text" value={this.state.lastName} name="lastName" />
+                </div>
+
+            </React.Fragment>
+
+        )
+    }
+}
