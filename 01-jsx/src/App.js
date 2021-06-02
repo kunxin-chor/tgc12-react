c5213d570d8c4df6ad1b47a248d2117bbbc2733f
kunxin-chor <chorkunxin@yahoo.com>
2021-06-02 09:28:32 +0000

    Added component example

--- 01-jsx/src/App.js
+++ 01-jsx/src/App.js
@@ -1,5 +1,6 @@
 import React from "react"
 import rabbit from "./rabbit.jpg"
+import cake from "./cake.jpg"
 import "./App.css"
 
 function App() {
@@ -10,22 +11,61 @@ function App() {
         "backgroundColor": "purple"
       }}>Hello World</p>
       <p>Another Hello</p>
-      <AlertBox></AlertBox>
-      <AlertBox></AlertBox>
+      <AlertBox message="AWAS!" color="blue"></AlertBox>
+      <AlertBox message="Hello from the other side"></AlertBox>
+      <Invert number={-100}/>
       <img src={require("./kitten.jpg").default}/>
       <img src={rabbit}/>
+      <BorderedImageFrame image={cake}/>
+      <BorderedImageFrame image={rabbit}/>
+      <AddTwo n1={2} n2={3}/>
     </React.Fragment>
   );
 }
 
-function AlertBox() {
+function Invert(props) {
   return (
     <React.Fragment>
-      <div>
-        Danger! Danger! Will Robinson!
+      <p>{props.number * -1}</p>
+    </React.Fragment>
+
+  )
+}
+
+function AlertBox(props) {
+  console.log(props)
+  let textColor = props.color || "red";
+  return (
+    <React.Fragment>
+      <div style={{"color": textColor}}>
+        {props.message}
       </div>
 
     </React.Fragment>
+  )
+}
+
+function BorderedImageFrame(props){
+  return (
+    <div style={{
+       borderColor: "red",
+       borderStyle:"solid",
+       borderWidth:"4px"
+    }}>
+      <p>Let there be cake</p>
+      <img src={props.image}/>
+    </div>
+
+  )
+}
+
+function AddTwo(props) {
+  let sum = props.n1 + props.n2;
+  return (
+    <React.Fragment>
+      <p>Sum is {sum}</p>
+    </React.Fragment>
+
   )
 }
 
