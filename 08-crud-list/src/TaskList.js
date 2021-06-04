05de5db01e7a3f7b073a363cb606f72497c90350
kunxin-chor <chorkunxin@yahoo.com>
2021-06-04 07:58:17 +0000

    Update checkbox

--- 08-crud-list/src/TaskList.js
+++ 08-crud-list/src/TaskList.js
@@ -31,6 +31,9 @@ export default class TaskList extends React.Component {
                         <input type="checkbox" 
                         checked={task.done}
                         className="me-3"
+                        onChange={()=>{
+                            this.updateDoneElegant(task.id);
+                        }}
                     />
                         {task.description}
                     </div>
@@ -38,6 +41,103 @@ export default class TaskList extends React.Component {
             )
         }
         return taskJSX;
+    }
+
+    updateDoneStraightfoward = (taskId) => {
+
+        // Find the task that we want to modify
+        let wantedTask = null;
+        for (let task of this.state.tasks) {
+            if (task.id == taskId) {
+                wantedTask = task;
+                break;
+            }
+        }
+
+        
+        // MODIFY AN OBJECT 
+        // clone the original object
+        let clonedTask = {...wantedTask};
+
+        // invert the done property of the clonedTask object
+        // if done was true, it will become false
+        // if done was false, it will become true
+
+        
+        // make the change to the object
+        clonedTask.done = !clonedTask.done;
+        
+        // same as:
+        // if (clonedTask.done) {
+        //
+        // clonedTask.done = false;
+        //} else{
+        // clonedTask.done = true;    
+        //}
+        //
+
+        // MODIFY THE TASK ARRAY
+        // clone the array
+        let cloned = this.state.tasks.slice();
+
+        // change the array
+        let indexToChange = this.state.tasks.findIndex(function(task){
+            return task.id == clonedTask.id;
+        })
+        cloned[indexToChange] = clonedTask;
+
+        // set cloned back into the state
+        this.setState({
+            'tasks': cloned
+        })
+    }
+
+    updateDoneElegant = (taskId) => {
+        let wantedTask = this.state.tasks.filter(function(task){
+            if (task.id == taskId) {
+                return task;
+            }
+        })[0];
+
+        let clonedTask = {...wantedTask};
+        clonedTask.done = !clonedTask.done;
+
+        // clone the original tasks array, and put the clonedTask into
+        // the position (i.e index) of the original task
+
+        let wantedIndex = this.state.tasks.findIndex(function(task){
+            return task.id == taskId
+        })
+
+        let clonedArray = [
+            ...this.state.tasks.slice(0, wantedIndex),
+            clonedTask,
+            ...this.state.tasks.slice(wantedIndex+1)
+        ]
+
+        this.setState({
+            'tasks': clonedArray
+        })
+    }
+
+    updateDoneIntermediate = (taskId) => {
+        let wantedTask = this.state.tasks.filter(task =>task.id == taskId);
+
+        let clonedTask = {...wantedTask};
+        clonedTask.done = !clonedTask.done;
+
+        let clonedArray = this.state.tasks.map(function(eachTask){
+            if (eachTask.id != taskId) {
+                return eachTask;
+            } else {
+                // if the id matches, then eachTask is the one that we want to modify
+                return clonedTask
+            }
+        })
+
+        this.setState({
+            'tasks': clonedArray
+        })
     }
 
     updateTaskName = (event) => {
